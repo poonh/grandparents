c9403e2ba7dcb11b4c7ac6611b3862bcec35d7c8
poonh <95615486+poonh@users.noreply.github.com>
2024-02-13 16:37:33 +0800

    Update login.php

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($entered_password === $password) {
         // Password is correct, grant access to the protected content
         // For demonstration purposes, you can redirect to another page
-        header("Location: protected_page.php");
+        header("Location: picturesecret.html");
         exit();
     } else {
         // Password is incorrect, display an error message
